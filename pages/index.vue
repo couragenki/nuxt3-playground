6c1af168c053ad557a9b7520de3aa118f713ad28
couragenki <genki.meado@gmail.com>
2022-01-11 14:54:18 +0900

    add sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,76 @@
 <template>
-    <div>
-        hoge
+  <div class="wrapper">
+    <div class="content hoge1">
+      hoge1
     </div>
+    <div class="content hoge2">
+      hoge2
+    </div>
+    <div class="movie">
+      <p class="animation-text">
+        hogehogehogehoge
+      </p>
+      <video
+        autoplay
+        muted
+        loop
+        src="https://www.apple.com/105/media/ww/imac-24/2021/5e004d75-3ad6-4bb9-ab59-41f891fc52f0/anim/display-hero/large_2x.mp4"
+      />
+    </div>
+    <div class="content hoge3">
+      hoge3
+    </div>
+    <div class="content hoge4">
+      hoge4
+    </div>
+  </div>
 </template>
+
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+    setup() {
+        console.log(defineComponent)       
+        console.log('test') 
+    },
+})
+</script>
+
+<style lang="scss">
+.wrapper {
+  width: 100%;
+  background-color: cornsilk;
+}
+.content {
+  width: 100%;
+  height: 2000px;
+}
+.movie {
+  width: 100%;
+}
+.movie video {
+  width: 100%;
+}
+.animation-text {
+  width: 100%;
+  position: absolute;
+  color: #FFF;
+  font-size: 100px;
+  text-align: center;
+}
+
+.hoge1 {
+  background-color: crimson;
+}
+.hoge2 {
+  background-color: yellow;
+}
+.hoge3 {
+  background-color: cyan;
+}
+.hoge4 {
+  background-color: blue;
+}
+</style>
